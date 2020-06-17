b4dbe3a46fc125cc267d6cf2dc81f6d6c0f18fc2
Santiago Sanchez-Ramirez <santiago.snchez@gmail.com>
2020-06-17 14:47:37 -0400

    updated to personal client info

--- handlers_drive.go
+++ handlers_drive.go
@@ -14,8 +14,8 @@ import (
 	"github.com/prasmussen/gdrive/drive"
 )
 
-const ClientId = "367116221053-7n0vf5akeru7on6o2fjinrecpdoe99eg.apps.googleusercontent.com"
-const ClientSecret = "1qsNodXNaWq1mQuBjUjmvhoO"
+const ClientId = "448411699400-4av4eej4vrme2uta58dmj1mj89qg5l6k.apps.googleusercontent.com"
+const ClientSecret = "JJQ2AmxGaySUaCj2TWsgSzoc"
 const TokenFilename = "token_v2.json"
 const DefaultCacheFileName = "file_cache.json"
 
